3836478673cf9a2cb76306c8dafe063214b9e7ac
james8115 <james8115@gmail.com>
2020-10-23 05:53:33 +0800

    search list by name

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../shared/Appointment';
 import { AppointmentService } from './../shared/appointment.service';
+import { FormControl } from "@angular/forms";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -10,10 +12,14 @@ import { AppointmentService } from './../shared/appointment.service';
 
 export class HomePage implements OnInit {
   Bookings = [];
+  Result = [];
+  public searchControl: FormControl;
 
   constructor(
     private aptService: AppointmentService
-  ) { }
+  ) {
+    this.searchControl = new FormControl();
+  }
 
   ngOnInit() {
     this.fetchBookings();
@@ -24,8 +30,16 @@ export class HomePage implements OnInit {
         let a = item.payload.toJSON();
         a['$key'] = item.key;
         this.Bookings.push(a as Appointment);
+        this.Result = this.Bookings;
       })
-    })
+    });
+    this.setFilteredItems("");
+    this.searchControl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(search => {
+        console.log(search)
+        this.setFilteredItems(search);
+      });
   }
 
   fetchBookings() {
@@ -40,4 +54,15 @@ export class HomePage implements OnInit {
       this.aptService.deleteBooking(id)
     }
   }
+
+  setFilteredItems(search) {
+    if (search===null){
+      this.Result = this.Bookings
+    }
+    else{
+    this.Result =  this.Bookings.filter(item => {
+      return item.name.toLowerCase().indexOf(search.toLowerCase()) > -1;
+    });
+    console.log(this.Result)
+  }}
 }
