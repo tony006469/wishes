ca690ae5892bc3abb62d6eaf77e742074669e4a3
unknown <james8115@gmail.com>
2023-01-16 21:45:30 +0800

    prevent double click when submit form

--- src/app/make-appointment/make-appointment.page.ts
+++ src/app/make-appointment/make-appointment.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder } from "@angular/forms";
 import { AppointmentService } from './../shared/appointment.service';
-import {formatDate} from '@angular/common';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-make-appointment',
@@ -11,14 +11,16 @@ import {formatDate} from '@angular/common';
 })
 export class MakeAppointmentPage implements OnInit {
   bookingForm: FormGroup;
+  disable: Boolean
 
   constructor(
     private aptService: AppointmentService,
     private router: Router,
-    public fb: FormBuilder
+    public fb: FormBuilder,
   ) { }
 
   ngOnInit() {
+    this.disable = false;
     this.bookingForm = this.fb.group({
       serial_number: [''],
       name: [''],
@@ -27,10 +29,10 @@ export class MakeAppointmentPage implements OnInit {
       address_number: [''],
       address: [''],
       money: [''],
-      create_date:[''],
-      expired_date:[''],
+      create_date: [''],
+      expired_date: [''],
       option: [''],
-      other:['']
+      other: ['']
     })
   }
 
@@ -45,25 +47,28 @@ export class MakeAppointmentPage implements OnInit {
     // console.log(nowDate.toLocaleDateString())
     // console.log(endDate)
     // this.bookingForm.controls['create_date'].setValue(nowDate.toLocaleDateString())
-    // this.bookingForm.controls['expired_date'].setValue(endDate)
+    // this.bookingForm.controls['expired_date'].setValue(endDate)    
+    this.disable = true
     var createDate = this.bookingForm.controls['create_date'].value
     var expireDate = this.bookingForm.controls['expired_date'].value
     var startDate = formatDate(createDate, 'yyyy/MM/dd', 'en-US')
     var endDate = formatDate(expireDate, 'yyyy/MM/dd', 'en-US')
     this.bookingForm.controls['create_date'].setValue(startDate)
     this.bookingForm.controls['expired_date'].setValue(endDate)
-
     if (!this.bookingForm.valid) {
       return false;
     } else {
-      this.aptService.createBooking(this.bookingForm.value).then(res => {
-        console.log(res)
-        this.bookingForm.reset();
-        this.aptService.order_number--;
-        this.aptService.updateOrder(this.aptService.order_number);
-        this.router.navigate(['/home']);
-      })
-        .catch(error => console.log(error));
+      if (!this.disable) {
+        this.aptService.createBooking(this.bookingForm.value).then(res => {
+          console.log(res)
+          this.bookingForm.reset();
+          this.aptService.order_number--;
+          this.aptService.updateOrder(this.aptService.order_number);
+          this.router.navigate(['/home']);
+        })
+          .catch(error => console.log(error));
+        this.disable = false
+      }
     }
   }
 }
